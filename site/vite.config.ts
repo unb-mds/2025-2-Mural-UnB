6f241badb66a88538d9843ca4d54a952b2aacf5b
Maria Clara <232005352@aluno.unb.br>
2025-10-14 14:08:19 -0300

    fix: Reestruturar arquitetura #50

---
+++ site/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  base: 'https://unb-mds.github.io/2025-2-Mural-UnB/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // com isso basta importar com @/'pasta dentro do src'/'item'
+    },
+  },
+})
