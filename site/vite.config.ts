168d8897787e8b2a39bda0d872248350ad637f4c
Maria <232005352@aluno.unb.br>
2025-10-20 23:33:11 -0300

    Fix: Deploy fix attempt at vite.config.ts

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
 import path from 'path'
-=======
->>>>>>> 17e311137a9a8bc594ce68d30aa61eb31bdaee8b
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,7 +16,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // com isso basta importar com @/'pasta dentro do src'/'item'
     },
   },
-=======
   base: '/2025-2-Mural-UnB/',
->>>>>>> 17e311137a9a8bc594ce68d30aa61eb31bdaee8b
 })
