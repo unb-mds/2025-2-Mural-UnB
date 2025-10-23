147dc0bd9c5d3d02d8c61d865567fb67e31a7fc5
Karmantinedev <165739233+Karmantinedev@users.noreply.github.com>
2025-10-22 22:12:04 -0300

    Update para config_ej.py
    O caminho apresentado no arquivo agora consolida as informações adquiridas no diretório correto em "script"

--- scripts/config_ej.py
+++ scripts/config_ej.py
@@ -7,7 +7,7 @@ GEMINI_API_KEY = os.getenv('GEMINI_API_KEY', 'sua_chave_aqui')
 PDF_URL_EJS = "https://unb.br/images/Noticias/2023/Documentos/PORTFLIO_EJS.pdf"
 
 # Configurações de saída
-OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "data", "EJs")
+OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "..", "data", "EJs")
 OUTPUT_JSON = "empresas_juniores_consolidadas.json"
 
 # Configurações do processamento
@@ -15,3 +15,4 @@ PROCESSAR_POR_PAGINA = True
 MAX_PAGINAS_POR_REQUISICAO = 2
 # pagina onde o processo começa, aqui pq a página 10 é o sumário ent evita erros
 PAGINA_INICIAL_EJS = 11  
+
