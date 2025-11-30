5d4302f7bdd0894db6c1f6f8ea67526798b47380
Tiago Bittencourt <tiago.bittencourt.2005@gmail.com>
2025-11-30 01:23:24 -0300

    fix: Vite config para usar dynamic base URL (deploy)
    #138

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
+// O nome do repositório. O GH Pages serve o site neste subdiretório.
+const REPO_NAME = '2025-2-Mural-UnB'; 
+
 export default defineConfig({
   plugins: [
     react(),
@@ -12,9 +14,7 @@ export default defineConfig({
   server: {
     open: false,
   },
-  base: process.env.NODE_ENV === 'production' 
-    ? 'https://unb-mds.github.io/2025-2-Mural-UnB/' 
-    : '/',
+  base: `/${REPO_NAME}/`, 
   resolve: {
     alias: {
       components: path.resolve(__dirname, "src/components"),
